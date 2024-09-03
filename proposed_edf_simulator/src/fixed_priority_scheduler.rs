1c6c43af2205ffeaf3df1dfe9b267029d67d484e
atsushi421 <atsushi.yano.2@tier4.jp>
2024-09-04 06:01:23 +0900

    feat: add fixed_priority_scheduler
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

---
+++ proposed_edf_simulator/src/fixed_priority_scheduler.rs
@@ -0,0 +1,34 @@
+use crate::dag_set_scheduler::DAGSetSchedulerBase;
+use crate::getset_dag_set_scheduler;
+use crate::{
+    graph_extension::NodeData, homogeneous::HomogeneousProcessor, log::DAGSetSchedulerLog,
+    processor::ProcessorBase,
+};
+use petgraph::graph::Graph;
+
+pub struct FixedPriorityScheduler {
+    dag_set: Vec<Graph<NodeData, i32>>,
+    processor: HomogeneousProcessor,
+    log: DAGSetSchedulerLog,
+    current_time: i32,
+}
+
+impl DAGSetSchedulerBase<HomogeneousProcessor> for FixedPriorityScheduler {
+    fn new(dag_set: &[Graph<NodeData, i32>], processor: &HomogeneousProcessor) -> Self {
+        Self {
+            dag_set: dag_set.to_vec(),
+            processor: processor.clone(),
+            log: DAGSetSchedulerLog::new(dag_set, processor.get_number_of_cores()),
+            current_time: 0,
+        }
+    }
+
+    fn sort_ready_queue(&self, ready_queue: &mut std::collections::VecDeque<NodeData>) {
+        ready_queue.make_contiguous().sort_by(|a, b| {
+            a.get_params_value("priority")
+                .cmp(&b.get_params_value("priority"))
+        });
+    }
+
+    getset_dag_set_scheduler!(HomogeneousProcessor);
+}
--- proposed_edf_simulator/src/lib.rs
+++ proposed_edf_simulator/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod core;
 pub mod dag_creator;
 pub mod dag_set_scheduler;
+pub mod fixed_priority_scheduler;
 pub mod graph_extension;
 pub mod homogeneous;
 pub mod log;
