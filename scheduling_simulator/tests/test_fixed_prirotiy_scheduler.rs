30f17f608b05ec65179c20a04ef2059c4ab02504
atsushi421 <atsushi.yano.2@tier4.jp>
2024-09-05 07:26:48 +0900

    test: fixed_priority_scheduler
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

--- scheduling_simulator/tests/test_fixed_prirotiy_scheduler.rs
+++ scheduling_simulator/tests/test_fixed_prirotiy_scheduler.rs
@@ -7,11 +7,39 @@ use scheduling_simulator::{
     },
     task::dag::DAG,
 };
-use util::create_sequential_dag_set;
+
+use util::{create_sequential_dag_set0, create_sequential_dag_set1};
+
+#[test]
+fn test_sequential_rm_scheduler_normal() {
+    let mut dag_set = create_sequential_dag_set1();
+    for dag in dag_set.iter_mut() {
+        let dag_period = dag.get_dag_period();
+        for node in dag.node_weights_mut() {
+            node.params.insert("priority".to_string(), dag_period);
+        }
+    }
+
+    let processor = HomogeneousProcessor::new(1);
+    let mut scheduler = FixedPriorityScheduler::new(&dag_set, &processor);
+    scheduler.schedule(
+        PreemptiveType::Preemptive {
+            key: "priority".to_string(),
+        },
+        27,
+    );
+
+    let log = scheduler.get_log();
+    assert!(!log.deadline_missed);
+    let rt0 = log.dag_set_log[0].response_times.clone();
+    assert_eq!(rt0, vec![1, 1, 1, 1, 1, 1]);
+    let rt1 = log.dag_set_log[1].response_times.clone();
+    assert_eq!(rt1, vec![5, 5, 5, 4]);
+}
 
 #[test]
 fn test_sequential_rm_scheduler_missed() {
-    let mut dag_set = create_sequential_dag_set();
+    let mut dag_set = create_sequential_dag_set0();
     for dag in dag_set.iter_mut() {
         let dag_period = dag.get_dag_period();
         for node in dag.node_weights_mut() {
--- scheduling_simulator/tests/util.rs
+++ scheduling_simulator/tests/util.rs
@@ -5,7 +5,7 @@ use scheduling_simulator::task::dag::{Node, DAG};
 
 // http://retis.sssup.it/~giorgio/paps/2005/rtsj05-rmedf.pdf
 // Figure 1
-pub fn create_sequential_dag_set() -> Vec<Graph<Node, i32>> {
+pub fn create_sequential_dag_set0() -> Vec<Graph<Node, i32>> {
     let mut dag_set = Vec::new();
 
     let mut dag = Graph::<Node, i32>::new();
@@ -13,6 +13,32 @@ pub fn create_sequential_dag_set() -> Vec<Graph<Node, i32>> {
         ("period".to_string(), 5),
         ("relative_deadline".to_string(), 5),
         ("execution_time".to_string(), 2),
+    ]);
+    dag.add_node(Node::new(0, params));
+    dag.set_param_to_all_nodes("dag_id", 0);
+    dag_set.push(dag);
+
+    let mut dag = Graph::<Node, i32>::new();
+    let params = BTreeMap::from([
+        ("period".to_string(), 7),
+        ("relative_deadline".to_string(), 7),
+        ("execution_time".to_string(), 4),
+    ]);
+    dag.add_node(Node::new(0, params));
+    dag.set_param_to_all_nodes("dag_id", 1);
+    dag_set.push(dag);
+
+    dag_set
+}
+
+pub fn create_sequential_dag_set1() -> Vec<Graph<Node, i32>> {
+    let mut dag_set = Vec::new();
+
+    let mut dag = Graph::<Node, i32>::new();
+    let params = BTreeMap::from([
+        ("period".to_string(), 5),
+        ("relative_deadline".to_string(), 5),
+        ("execution_time".to_string(), 1),
     ]);
     dag.add_node(Node::new(0, params));
     dag.set_param_to_all_nodes("dag_id", 0);
