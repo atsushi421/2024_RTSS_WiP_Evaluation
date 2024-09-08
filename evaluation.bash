ad807fde862cf92ef83a1507446a450137f04fa2
atsushi421 <atsushi.yano.2@tier4.jp>
2024-09-08 10:37:48 +0900

    fix: evaluation

--- evaluation.bash
+++ evaluation.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ALGS=( "proposed-edf" "rm" "greedy" )
 NUM_CORES=4
 SIM_DURATION=1000000
 
@@ -8,5 +7,5 @@ cd /home/atsushi/2024_RTSS_WiP_Evaluation/scheduling_simulator
 cargo build --release
 for alg in "${ALGS[@]}"
 do
-    RUST_BACKTRACE=1 /home/atsushi/2024_RTSS_WiP_Evaluation/scheduling_simulator/target/release/scheduling_simulator -a $alg -c $NUM_CORES -s $SIM_DURATION
+    RUST_BACKTRACE=1 /home/atsushi/2024_RTSS_WiP_Evaluation/scheduling_simulator/target/release/scheduling_simulator -c $NUM_CORES -s $SIM_DURATION
 done
