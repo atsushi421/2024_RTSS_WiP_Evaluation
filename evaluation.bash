2046b1b96b6421283fb355d37790811e35043a9e
atsushi421 <atsushi.yano.2@tier4.jp>
2024-09-10 21:00:33 +0900

    fix: path in evaluation.bash

--- evaluation.bash
+++ evaluation.bash
@@ -4,7 +4,7 @@ NUM_CORES=7
 SIM_DURATION=3000000
 NUM_SIM=5000
 
-cd /home/atsushi/2024_RTSS_WiP_Evaluation/scheduling_simulator
+cd $HOME/2024_RTSS_WiP_Evaluation/scheduling_simulator
 cargo build --release
 export RAYON_NUM_THREADS=24
-/home/atsushi/2024_RTSS_WiP_Evaluation/scheduling_simulator/target/release/scheduling_simulator -c $NUM_CORES -s $SIM_DURATION -r $NUM_SIM
+$HOME/2024_RTSS_WiP_Evaluation/scheduling_simulator/target/release/scheduling_simulator -c $NUM_CORES -s $SIM_DURATION -r $NUM_SIM
