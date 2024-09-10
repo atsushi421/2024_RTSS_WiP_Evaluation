8894af9cb0845781f98f2ddc4670531c8e83e4ae
atsushi421 <atsushi.yano.2@tier4.jp>
2024-09-10 20:44:02 +0900

    update evaluation.bash

--- evaluation.bash
+++ evaluation.bash
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-NUM_CORES=4
-SIM_DURATION=1000000
+NUM_CORES=7
+SIM_DURATION=3000000
+NUM_SIM=1000
 
 cd /home/atsushi/2024_RTSS_WiP_Evaluation/scheduling_simulator
 cargo build --release
-for alg in "${ALGS[@]}"
-do
-    RUST_BACKTRACE=1 /home/atsushi/2024_RTSS_WiP_Evaluation/scheduling_simulator/target/release/scheduling_simulator -c $NUM_CORES -s $SIM_DURATION
-done
+export RAYON_NUM_THREADS=24
+/home/atsushi/2024_RTSS_WiP_Evaluation/scheduling_simulator/target/release/scheduling_simulator -c $NUM_CORES -s $SIM_DURATION -r $NUM_SIM
